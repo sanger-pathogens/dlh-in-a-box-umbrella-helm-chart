5eb48b316e496b3f1193944eaa7bd28a24dd3fd5
Jacob Dicken <jacobdicken2005@gmail.com>
2026-08-16 21:16:43 +0100

    Fix script paths in lint.sh

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -4,9 +4,9 @@ set -euo pipefail
 ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 
 bash -n "${ROOT_DIR}"/hack/*.sh
-"${ROOT_DIR}"/hack/license-check.sh
-"${ROOT_DIR}"/hack/docs-check.sh
-"${ROOT_DIR}"/hack/security-check.sh
+"${ROOT_DIR}"/scripts/license-check.sh
+"${ROOT_DIR}"/scripts/docs-check.sh
+"${ROOT_DIR}"/scripts/security-check.sh
 "${ROOT_DIR}"/test/render-contract.sh
-"${ROOT_DIR}"/hack/helm-lint.sh
+"${ROOT_DIR}"/scripts/helm-lint.sh
 
